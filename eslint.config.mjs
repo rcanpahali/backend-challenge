e9303c8607edd11a1a15eb564d7fa1622fc54bcc
rcanpahali <canpahali@gmail.com>
2026-05-25 14:08:47 +0200

    fix(prettier): revert changes on `world_data.json` file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import prettier from "eslint-config-prettier";
 import pluginPrettier from "eslint-plugin-prettier";
 
 export default tseslint.config(
-  { ignores: ["dist/**", "node_modules/**"] },
+  { ignores: ["dist/**", "node_modules/**", "src/data/**"] },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   prettier,
